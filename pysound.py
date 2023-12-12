10c8a361ac027a4521e4b712a1978fa417a61151
tct123 <42028373+tct123@users.noreply.github.com>
2023-12-12 20:33:19 +0100

    Update pysound.py

--- pysound.py
+++ pysound.py
@@ -9,10 +9,12 @@ def pysound(file):
         print("IOS is not supported at the moment")
     else:
         import pygame
+
         pygame.mixer.init()
         pygame.mixer.music.load(filename=file)
         pygame.mixer.music.set_volume(1.0)
         pygame.mixer.music.play()
+
 
 if __name__ == "__main__":
     pysound(file="happy-birthday-whistled.wav")
